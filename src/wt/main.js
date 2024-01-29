94ae50c62a8dee56e385e903334c0bd239a1d2be
Olga <cornflower1989@gmail.com>
2024-01-29 18:41:48 +0300

    feat: implement worker threads

--- src/wt/main.js
+++ src/wt/main.js
@@ -1,5 +1,29 @@
+import { Worker } from 'worker_threads';
+import { resolve } from 'path';
+import { cpus } from 'os';
+
+const workerPath = resolve('src', 'wt', 'worker.js');
+const initialValues = cpus().map((el, i) => el = i + 10);
+
+const calculateInWorker = (value) => {
+    return new Promise((resolve) => {
+        const worker = new Worker(workerPath, { workerData: value });
+
+        worker.on('message', result => resolve({
+            status: 'resolved',
+            data: result,
+        }));
+        worker.on('error', () => resolve({
+            status: 'error',
+            data: null,
+        }));
+    })
+}
+
 const performCalculations = async () => {
-    // Write your code here
+    const workerPromises = [];
+    initialValues.forEach(value => workerPromises.push(calculateInWorker(value)));
+    Promise.all(workerPromises).then(result => console.log(result));
 };
 
 await performCalculations();
