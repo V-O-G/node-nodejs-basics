f2dc89a141babd654d3f12112258f72acb0168bd
Olga <cornflower1989@gmail.com>
2024-01-29 10:43:50 +0300

    feat: implement streams

--- src/streams/read.js
+++ src/streams/read.js
@@ -1,5 +1,10 @@
+import { resolve } from 'path';
+import { createReadStream } from 'fs';
+
+const filePath = resolve('src', 'streams', 'files', 'fileToRead.txt');
+
 const read = async () => {
-    // Write your code here 
+    createReadStream(filePath).pipe(process.stdout);
 };
 
 await read();
